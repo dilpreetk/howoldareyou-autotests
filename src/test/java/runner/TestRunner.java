5f0432f1324b5ef4ec2502f09052a6da9ae10b7a
Dilpreet Kaur <dilprit95@gmail.com>
2023-07-12 16:41:53 +0100

    fix test config

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,16 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.junit.runner.RunWith;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features ="src/main/resources/feature",
+		glue= {"com.howoldareyou.step"},
+		tags = ""
+		)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
